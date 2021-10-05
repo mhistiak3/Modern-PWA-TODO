1ac6b708a3b9cb25bf8ddaf810182cd801ac6ad4
Istiak <mhistiak2@gmail.com>
2021-10-05 23:18:26 +0600

    Fixed Bug in Service Worker

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -4,13 +4,13 @@ this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
-        "/static/js/bundle.js",
-        "/static/js/main.chunk.js",
-        "/static/js/vendors~main.chunk.js",
+        "https://ia-todo.netlify.app/static/js/2.3c51dae2.chunk.js",
+        "https://ia-todo.netlify.app/static/js/main.7f98cf7a.chunk.js",
+        "https://ia-todo.netlify.app/static/css/main.38512767.chunk.css",
         "index.html",
-        "/",
         "/manifest.json",
         "/static/media/logo.29f81146.png",
+        "https://ia-todo.netlify.app/",
       ]);
     })
   );
