38313bf052f59ac30b98b40a6226cc8f0fc289f6
Istiak <mhistiak2@gmail.com>
2022-01-17 21:57:42 +0600

    Add Filter Option

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -1,12 +1,12 @@
-let cacheData = "appV1";
+let cacheData = "todo_v_1.0.0";
 // Event for caches files for runnig in offline
 this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
         "https://ia-todo.netlify.app/static/js/2.3c51dae2.chunk.js",
-        "https://ia-todo.netlify.app/static/js/main.f3231e17.chunk.js",
-        "https://ia-todo.netlify.app/static/css/main.be24d390.chunk.css",
+        "https://ia-todo.netlify.app/static/js/main.1aacd3ad.chunk.js",
+        "https://ia-todo.netlify.app/static/css/main.dfc17785.chunk.css",
         "index.html",
         "/manifest.json",
         "/static/media/logo.29f81146.png",
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,18 +2,39 @@ import classes from "../style/TodoList.module.css";
 import AddIcon from "./AddIcon";
 import Todo from "./Todo";
 import { Scrollbars } from "react-custom-scrollbars";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../context";
 
 const TodoList = () => {
-
+  const [state, setState] = useState("All");
   let { TodosList, length } = useContext(Context);
+
+  
+  const filterTodo = TodosList.filter((todo) => {
+    if (state === "All") {
+      return todo;
+    } else if (state === "Completed") {
+      return todo.completed === true;
+    } else if (state === "Incompleted") {
+      return todo.completed === false;
+    } else {
+      return todo;
+    }
+  });
+console.log(filterTodo);
   return (
     <div className={classes.todoList}>
-      <h1>INBOX</h1>
+      <div className={classes.todoListHeader}>
+        <h1>INBOX</h1>
+        <select onChange={(e) => setState(e.target.value)}>
+          <option value="All">All</option>
+          <option value="Completed">Completed</option>
+          <option value="Incompleted">Incompleted</option>
+        </select>
+      </div>
       <Scrollbars style={{ height: "350px" }}>
         {TodosList.length > 0 ? (
-          TodosList.map((MyTodo) => <Todo task={MyTodo} key={MyTodo.id} />)
+          filterTodo.map((MyTodo) => <Todo task={MyTodo} key={MyTodo.id} />)
         ) : (
           <h2>No Task Found</h2>
         )}
